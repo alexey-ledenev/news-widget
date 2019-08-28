7d6cd305a49c707f1035239fdb38f5f1c78b0867
Alexey L <alex-itcom@yandex.ru>
2019-08-28 18:11:20 +0300

    Добавлен fetch-запрос

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <carousel :itemsCount="2" :items="[]" />
+    <carousel :itemsCount="2" :items="items" />
   </div>
 </template>
 
@@ -14,8 +14,47 @@ import Carousel from './components/carousel.vue';
   },
 })
 export default class App extends Vue {
-  mounted() {
-    // fetching data
+  private limit = 2
+
+  private offset = 0
+
+  private isNext = true
+
+  private items = []
+
+  private fetching() {
+    const url = `https://prizyv31.ru/edw/api/data-marts/57/entities.json?limit=${this.limit}&offset=${this.offset}`
+    return fetch('', {
+        method: 'GET',
+        mode: 'cors',
+    })
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+        return Promise.reject(new Error('Network request failed with response ' + response.status + ': ' + response.statusText))
+      }
+    })
+  }
+
+  private processing(data) {
+    if (!data.next) this.isNext = false
+    const itms = [...data.results.objects]
+    if (itms.length) {
+      itms.forEach(itm => this.items.push(itm))
+    }
+  }
+
+  private getItems() {
+    if (this.isNext) {
+      this.fetching()
+        .then(data => this.processing(data))
+        .catch(error => console.error(error))
+    }
+  }
+
+  private mounted() {
+    this.getItems()
   }
 }
 </script>
